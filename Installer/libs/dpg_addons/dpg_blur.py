593093f8b1294697e63d8dfbf2e259cd076bbd6f
Agzes <103037173+Agzes@users.noreply.github.com>
2024-10-31 03:35:34 +1200

    🧊| dpg_blur update

--- Installer/libs/dpg_addons/dpg_blur.py
+++ Installer/libs/dpg_addons/dpg_blur.py
@@ -2,66 +2,24 @@ from ctypes import POINTER, c_bool, c_int, cdll, pointer, sizeof, Structure, byr
 from ctypes.wintypes import DWORD, LONG, LPCVOID, ULONG
 import ctypes.wintypes
 from ctypes import wintypes
-from enum import Enum
 import os
 
-class WINDOWCOMPOSITIONATTRIB(Enum):
-    WCA_UNDEFINED = 0
-    WCA_NCRENDERING_ENABLED = 1
-    WCA_NCRENDERING_POLICY = 2
-    WCA_TRANSITIONS_FORCEDISABLED = 3
-    WCA_ALLOW_NCPAINT = 4
-    WCA_CAPTION_BUTTON_BOUNDS = 5
-    WCA_NONCLIENT_RTL_LAYOUT = 6
-    WCA_FORCE_ICONIC_REPRESENTATION = 7
-    WCA_EXTENDED_FRAME_BOUNDS = 8
-    WCA_HAS_ICONIC_BITMAP = 9
-    WCA_THEME_ATTRIBUTES = 10
-    WCA_NCRENDERING_EXILED = 11
-    WCA_NCADORNMENTINFO = 12
-    WCA_EXCLUDED_FROM_LIVEPREVIEW = 13
-    WCA_VIDEO_OVERLAY_ACTIVE = 14
-    WCA_FORCE_ACTIVEWINDOW_APPEARANCE = 15
-    WCA_DISALLOW_PEEK = 16
-    WCA_CLOAK = 17
-    WCA_CLOAKED = 18
-    WCA_ACCENT_POLICY = 19
-    WCA_FREEZE_REPRESENTATION = 20
-    WCA_EVER_UNCLOAKED = 21
-    WCA_VISUAL_OWNER = 22
-    WCA_HOLOGRAPHIC = 23
-    WCA_EXCLUDED_FROM_DDA = 24
-    WCA_PASSIVEUPDATEMODE = 25
-    WCA_USEDARKMODECOLORS = 26
-    WCA_CORNER_STYLE = 27
-    WCA_PART_COLOR = 28
-    WCA_DISABLE_MOVESIZE_FEEDBACK = 29
-    WCA_LAST = 30
-class ACCENT_STATE(Enum):
-    """ Client area status enumeration class """
-    ACCENT_DISABLED = 0
-    ACCENT_ENABLE_GRADIENT = 1
-    ACCENT_ENABLE_TRANSPARENTGRADIENT = 2
-    ACCENT_ENABLE_BLURBEHIND = 3           # Aero effect
-    ACCENT_ENABLE_ACRYLICBLURBEHIND = 4    # Acrylic effect
-    ACCENT_ENABLE_HOSTBACKDROP = 5         # Mica effect
-    ACCENT_INVALID_STATE = 6
-class ACCENT_POLICY(Structure):
-    """ Specific attributes of client area """
 
+class ACCENT_POLICY(Structure):
     _fields_ = [
         ("AccentState",     DWORD),
         ("AccentFlags",     DWORD),
         ("GradientColor",   DWORD),
         ("AnimationId",     DWORD),
     ]
+
 class WINDOWCOMPOSITIONATTRIBDATA(Structure):
     _fields_ = [
         ("Attribute",   DWORD),
-        # Pointer() receives any ctypes type and returns a pointer type
         ("Data",        POINTER(ACCENT_POLICY)),
         ("SizeOfData",  ULONG),
     ]
+
 class MARGINS(Structure):
     _fields_ = [
         ("cxLeftWidth",     c_int),
@@ -69,77 +27,41 @@ class MARGINS(Structure):
         ("cyTopHeight",     c_int),
         ("cyBottomHeight",  c_int),
     ]
-user32 = ctypes.windll.user32
-WNDENUMPROC = ctypes.WINFUNCTYPE(ctypes.wintypes.BOOL,
-                                 ctypes.wintypes.HWND,
-                                 ctypes.wintypes.LPARAM)
-user32.EnumWindows.argtypes = [WNDENUMPROC,ctypes.wintypes.LPARAM]
-class WindowsWindowEffect:
-    """ Windows window effect """
 
+user32 = ctypes.windll.user32
+WNDENUMPROC = ctypes.WINFUNCTYPE(ctypes.wintypes.BOOL, ctypes.wintypes.HWND, ctypes.wintypes.LPARAM)
+user32.EnumWindows.argtypes = [WNDENUMPROC,ctypes.wintypes.LPARAM]
+
+class WindowsWindowEffect:
     def __init__(self):
-        # Declare the function signature of the API
         self.user32 = cdll.LoadLibrary("user32")
         self.dwmapi = cdll.LoadLibrary("dwmapi")
         self.SetWindowCompositionAttribute = self.user32.SetWindowCompositionAttribute
-        self.DwmExtendFrameIntoClientArea = self.dwmapi.DwmExtendFrameIntoClientArea
         self.DwmSetWindowAttribute = self.dwmapi.DwmSetWindowAttribute
-        self.SetWindowCompositionAttribute.restype = c_bool
-        self.DwmExtendFrameIntoClientArea.restype = LONG
-        self.DwmSetWindowAttribute.restype = LONG
-        self.SetWindowCompositionAttribute.argtypes = [
-            c_int,
-            POINTER(WINDOWCOMPOSITIONATTRIBDATA),
-        ]
-        self.DwmSetWindowAttribute.argtypes = [c_int, DWORD, LPCVOID, DWORD]
-        self.DwmExtendFrameIntoClientArea.argtypes = [c_int, POINTER(MARGINS)]
-
-        # Initialize structure
         self.accentPolicy = ACCENT_POLICY()
         self.winCompAttrData = WINDOWCOMPOSITIONATTRIBDATA()
-        self.winCompAttrData.Attribute = WINDOWCOMPOSITIONATTRIB.WCA_ACCENT_POLICY.value
+        self.winCompAttrData.Attribute = 19
         self.winCompAttrData.SizeOfData = sizeof(self.accentPolicy)
         self.winCompAttrData.Data = pointer(self.accentPolicy)
-
-    
     def setAeroEffect(self, hWnd):
-        """ Add the aero effect to the window
-
-        Parameters
-        ----------
-        hWnd: int or `sip.voidptr`
-            Window handle
-        """
-        self.winCompAttrData.Attribute = WINDOWCOMPOSITIONATTRIB.WCA_ACCENT_POLICY.value
-        self.accentPolicy.AccentState = ACCENT_STATE.ACCENT_ENABLE_BLURBEHIND.value
+        self.winCompAttrData.Attribute = 19
+        self.accentPolicy.AccentState = 3
         self.SetWindowCompositionAttribute(hWnd, pointer(self.winCompAttrData))
-
     def setRoundedCorners(self, hWnd, radius):
-        """ Set rounded corners for the window
-
-        Parameters
-        ----------
-        hWnd: int or `sip.voidptr`
-            Window handle
-        radius: int
-            Radius of the corners
-        """
         self.DwmSetWindowAttribute(hWnd, wintypes.DWORD(33), byref(wintypes.INT(2)), wintypes.UINT(sizeof(wintypes.INT)))
 
-def get_hwnd_from_pid(pid: int):
+
+def get_hwnd():
     result = None
     def callback(hwnd, _):
         nonlocal result
         lpdw_PID = ctypes.c_ulong()
         user32.GetWindowThreadProcessId(hwnd, ctypes.byref(lpdw_PID))
         hwnd_PID = lpdw_PID.value
-
-        if hwnd_PID == pid:
+        if hwnd_PID == os.getpid():
             result = hwnd
             return False
         return True
     cb_worker = WNDENUMPROC(callback)
     user32.EnumWindows(cb_worker, 0)
     return result
-def get_hwnd():
-    return get_hwnd_from_pid(os.getpid())
