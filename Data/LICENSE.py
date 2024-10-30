b211df5e00e125761998ab4640d813de8571b0d2
Agzes <103037173+Agzes@users.noreply.github.com>
2024-10-31 03:31:48 +1200

    🛡️ | License

---
+++ Data/LICENSE.py
@@ -0,0 +1,155 @@
+License_RU = """## MIT License 
+
+Copyright (с) 2024 Agzes
+
+---
+ 
+Данная лицензия разрешает лицам, 
+получившим копию данного программного обеспечения и 
+сопутствующей документации (далее — «Программное Обеспечение»), 
+безвозмездно использовать Программное Обеспечение без ограничений
+, включая неограниченное право на использование, копирование, 
+изменение, слияние, публикацию, распространение, 
+сублицензирование и/или продажу копий Программного Обеспечения, 
+а также лицам, которым предоставляется данное ПО, при 
+соблюдении следующих условий:
+<text></text>
+Указанное выше уведомление об авторских правах и данное 
+разрешение должны быть включены во все копии или значимые 
+части данного Программного Обеспечения.
+
+---
+
+ДАННОЕ ПРОГРАММНОЕ ОБЕСПЕЧЕНИЕ ПРЕДОСТАВЛЯЕТСЯ «КАК ЕСТЬ», 
+БЕЗ КАКИХ-ЛИБО ГАРАНТИЙ, ЯВНЫХ ИЛИ ПОДРАЗУМЕВАЕМЫХ, ВКЛЮЧАЯ, 
+НО НЕ ОГРАНИЧИВАЯСЬ ГАРАНТИЯМИ ТОВАРНОЙ ПРИГОДНОСТИ, ПРИГОДНОСТИ 
+ДЛЯ ОПРЕДЕЛЕННОЙ ЦЕЛИ И НЕНАРУШЕНИЯ ПРАВ. В НИКАКОМ СЛУЧАЕ 
+АВТОРЫ И ПРАВООБЛАДАТЕЛИ НЕ НЕСУТ ОТВЕТСТВЕННОСТИ ПО ИСКАМ 
+О ВОЗМЕЩЕНИИ УЩЕРБА, УБЫТКОВ ИЛИ ДРУГИХ ТРЕБОВАНИЯМ, ВОЗНИКШИМ 
+В РЕЗУЛЬТАТЕ ИСПОЛЬЗОВАНИЯ ПРОГРАММНОГО ОБЕСПЕЧЕНИЯ ИЛИ 
+ДЕЙСТВИЙ В СООТВЕТСТВИИ С ПРОГРАММНЫМ ОБЕСПЕЧЕНИЕМ ИЛИ В СВЯЗИ 
+С ИМ.
+"""
+
+License_EN = """## MIT License 
+
+Copyright (с) 2024 Agzes
+---
+ 
+Permission is hereby granted, free of charge, to any person 
+obtaining a copy of this software and associated documentation 
+files (the "Software"), to deal in the Software without 
+restriction, including without limitation the rights to use, 
+copy, modify, merge, publish, distribute, sublicense, and/or 
+sell copies of the Software, and to permit persons to whom 
+the Software is furnished to do so, subject to the following 
+conditions:
+<text></text>
+The above copyright notice and this permission notice shall be 
+included in all copies or substantial portions of the Software.
+
+---
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE, ARISING 
+FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+License_RU_old = """## Условия использования Pc-Stat-Bot
+---        
+## Введение
+**Pc-Stat-Bot** - это многофункциональный бот для узнавания 
+разной информации пк удалённо. Он позволяет управлять музыкой,
+видео, звуком, мышкой, клавиатурой и другими функциями ПК через 
+телеграм-бота. Он также имеет графический интерфейс 
+пользователя (UI), который показывает статистику ПК, такую как 
+загрузка ЦП, загрузка ГП, загрузку ОП и т.д. **Pc-Stat-Bot** 
+разработан и принадлежит Agzes. Это приложение является 
+бесплатным и c открытым исходным кодом, который вы можете 
+найти на **github**.Вы можете использовать приложение 
+**Pc-Stat-Bot** только в личных и не коммерческих целях. 
+Вы не можете продавать,сдавать в аренду, распространять или 
+передавать приложение или его части третьим лицам.
+
+---
+## Область применения
+Вы можете модифицировать исходный код приложения соответствии 
+с лицензией **MIT License**, которая доступна на **github**. 
+Вы должны сохранять все авторские права.
+Вы несете полную ответственность за любые действия, которые вы 
+совершаете с помощью приложения **Pc-Stat-Bot**.
+
+---
+## Отказ от ответственности
+Мы предоставляем вам приложение **Pc-Stat-Bot** **как есть** без 
+каких-либо гарантий или обязательств. Мы не гарантируем, что 
+приложение будет работать без ошибок, сбоев или прерываний. 
+Мы также не гарантируем, что приложение будет соответствовать 
+вашим потребностям или ожиданиям.Мы не несем ответственности за 
+любые убытки или ущерб,который может возникнуть в связи с 
+использованием или невозможностью использования приложения 
+Pc-Stat-Bot. Это включает, но не ограничивается, потерей 
+данных, прибыли, репутации, деловой информации или другими 
+не материальными убытками. Мы также не несем ответственности 
+за любые претензии или  требования третьих лиц, связанные с 
+вашим использованием приложения **Pc-Stat-Bot**. 
+
+---
+## Изменение условий использования
+Мы оставляем за собой право изменять эти условия использования
+в любое время по нашему усмотрению. Мы будем уведомлять вас о 
+любых существенных изменениях через приложение или другие 
+доступные каналы. Ваше продолжение использования приложения 
+после такого уведомления будет означать ваше согласие с 
+измененными условиями использования."""
+
+License_EN_old = """## Pc-Stat-Bot: Terms of Use
+
+---
+## Introduction
+**Pc-Stat-Bot** is a multifunctional bot for recognizing different 
+pc information remotely. It allows you to control music, video, 
+sound, mouse, keyboard and other PC functions via telegram bot. 
+It also has a graphical user interface (UI) that shows PC
+statistics such as CPU utilization, GPU utilization, OP 
+utilization, etc. **Pc-Stat-Bot** is developed and owned by Agzes. 
+This application is free and open source which you can find on 
+**github**. You can use **Pc-Stat-Bot** application only for 
+personal and non commercial purposes. You may not sell, rent, 
+lease, distribute or transfer the application or parts of it 
+to third parties.
+
+---
+## Application Scope
+You may modify the source code of the application under the 
+**MIT License**, which is available on **github**. 
+You must retain all copyrights. You are solely 
+responsible for any actions you take with the **Pc-Stat-Bot** 
+application.
+
+---
+## Disclaimer
+We provide you with the **Pc-Stat-Bot** application **as is** 
+*as is* without any warranty or guarantee. We do not guarantee
+that the application will be error, crash or interruption free. 
+We also do not warrant that the application will meet your 
+needs or expectations.We will not be liable for any loss or 
+damage that may arise from the use or inability to use the 
+Pc-Stat-Bot application. This includes, but is not limited to, 
+loss of data, profits, reputation, business information or other 
+non-tangible damages. We are also not responsible for any third 
+party claims or demands arising from your use of the 
+**Pc-Stat-Bot application**.
+
+---
+## Change of Terms of Use
+We reserve the right to change these terms of use at any time 
+at our discretion. We will notify you of any material changes
+through the app or other available channels. Your continued 
+use of the app after such notification will constitute your 
+acceptance of the modified terms of use."""
